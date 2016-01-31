894ec2cd4decbe3ab27b2ceb8a3dd6a30b14eae3
fneu <fabian@neuschmidt.de>
2016-01-31 16:06:16 +0100

    DbusTest: Move mainloop creation to another module

--- coalib/tests/output/dbus/DbusTest.py
+++ coalib/tests/output/dbus/DbusTest.py
@@ -24,22 +24,7 @@ def make_test_server():
     # as that gives SegmentationFaults because the same bus is being used.
     return subprocess.Popen([
         sys.executable,
-        __file__,
-        "server"])
-
-
-def create_mainloop():
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
-    session_bus = dbus.SessionBus()
-    # The BusName needs to be saved to a variable, if it is not saved - the
-    # Bus will be closed.
-    dbus_name = dbus.service.BusName("org.coala_analyzer.v1.test", session_bus)
-    dbus_server = DbusServer(session_bus, "/org/coala_analyzer/v1/test",
-                             on_disconnected=lambda: GLib.idle_add(sys.exit))
-
-    mainloop = GLib.MainLoop()
-    mainloop.run()
+        os.path.join(os.path.dirname(__file__), "DbusTestHelper.py")])
 
 
 @skipIf(skip, message)
@@ -159,11 +144,4 @@ class DbusTest(unittest.TestCase):
 
 
 if __name__ == "__main__" and not skip:
-    arg = ""
-    if len(sys.argv) > 1:
-        arg = sys.argv[1]
-
-    if arg == "server":
-        create_mainloop()
-    else:
-        unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
